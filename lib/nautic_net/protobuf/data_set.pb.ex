dd00f1c503f96f72eef29a77b40297bc9bf0ea50
Rockwell Schrock <rockwell@schrock.me>
2023-01-11 15:48:50 -0500

    Rename everything

--- lib/nautic_net/protobuf/data_set.pb.ex
+++ lib/nautic_net/protobuf/data_set.pb.ex
@@ -1,10 +1,19 @@
-defmodule NauticNet.Protobuf.DirectionReference do
+defmodule NauticNet.Protobuf.AngleReference do
   @moduledoc false
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:DIRECTION_REFERENCE_NONE, 0)
-  field(:DIRECTION_REFERENCE_TRUE, 1)
-  field(:DIRECTION_REFERENCE_MAGNETIC, 2)
+  field(:ANGLE_REFERENCE_NONE, 0)
+  field(:ANGLE_REFERENCE_TRUE_NORTH, 1)
+  field(:ANGLE_REFERENCE_MAGNETIC_MORTH, 2)
+end
+
+defmodule NauticNet.Protobuf.SpeedReference do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field(:SPEED_REFERENCE_NONE, 0)
+  field(:SPEED_REFERENCE_GROUND, 1)
+  field(:SPEED_REFERENCE_WATER, 2)
 end
 
 defmodule NauticNet.Protobuf.WindReference do
@@ -12,11 +21,11 @@ defmodule NauticNet.Protobuf.WindReference do
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field(:WIND_REFERENCE_NONE, 0)
-  field(:WIND_REFERENCE_TRUE, 1)
-  field(:WIND_REFERENCE_MAGNETIC, 2)
+  field(:WIND_REFERENCE_TRUE_NORTH, 1)
+  field(:WIND_REFERENCE_MAGNETIC_NORTH, 2)
   field(:WIND_REFERENCE_APPARENT, 3)
-  field(:WIND_REFERENCE_TRUE_BOAT, 4)
-  field(:WIND_REFERENCE_TRUE_WATER, 5)
+  field(:WIND_REFERENCE_BOAT_TRUE_NORTH, 4)
+  field(:WIND_REFERENCE_WATER_TRUE_NORTH, 5)
 end
 
 defmodule NauticNet.Protobuf.DataSet.DataPoint do
@@ -28,18 +37,8 @@ defmodule NauticNet.Protobuf.DataSet.DataPoint do
   field(:timestamp, 1, type: Google.Protobuf.Timestamp)
   field(:hw_unique_number, 2, type: :uint32, json_name: "hwUniqueNumber")
   field(:heading, 16, type: NauticNet.Protobuf.HeadingSample, oneof: 0)
-
-  field(:speed_water_referenced, 17,
-    type: NauticNet.Protobuf.SpeedSample,
-    json_name: "speedWaterReferenced",
-    oneof: 0
-  )
-
-  field(:velocity_over_ground, 18,
-    type: NauticNet.Protobuf.VelocitySample,
-    json_name: "velocityOverGround",
-    oneof: 0
-  )
+  field(:speed, 17, type: NauticNet.Protobuf.SpeedSample, oneof: 0)
+  field(:velocity, 18, type: NauticNet.Protobuf.VelocitySample, oneof: 0)
 
   field(:wind_velocity, 19,
     type: NauticNet.Protobuf.WindVelocitySample,
@@ -75,31 +74,58 @@ defmodule NauticNet.Protobuf.HeadingSample do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:reference, 1, type: NauticNet.Protobuf.DirectionReference, enum: true)
-  field(:heading_rad, 2, type: :float, json_name: "headingRad")
+  field(:angle_reference, 1,
+    type: NauticNet.Protobuf.AngleReference,
+    json_name: "angleReference",
+    enum: true
+  )
+
+  field(:angle_rad, 2, type: :float, json_name: "angleRad")
 end
 
 defmodule NauticNet.Protobuf.SpeedSample do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:speed_m_s, 1, type: :float, json_name: "speedMS")
+  field(:speed_reference, 1,
+    type: NauticNet.Protobuf.SpeedReference,
+    json_name: "speedReference",
+    enum: true
+  )
+
+  field(:speed_m_s, 2, type: :float, json_name: "speedMS")
 end
 
 defmodule NauticNet.Protobuf.VelocitySample do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:reference, 1, type: NauticNet.Protobuf.DirectionReference, enum: true)
-  field(:speed_m_s, 2, type: :float, json_name: "speedMS")
-  field(:angle_rad, 3, type: :float, json_name: "angleRad")
+  field(:speed_reference, 1,
+    type: NauticNet.Protobuf.SpeedReference,
+    json_name: "speedReference",
+    enum: true
+  )
+
+  field(:angle_reference, 2,
+    type: NauticNet.Protobuf.AngleReference,
+    json_name: "angleReference",
+    enum: true
+  )
+
+  field(:speed_m_s, 3, type: :float, json_name: "speedMS")
+  field(:angle_rad, 4, type: :float, json_name: "angleRad")
 end
 
 defmodule NauticNet.Protobuf.WindVelocitySample do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:reference, 1, type: NauticNet.Protobuf.WindReference, enum: true)
+  field(:wind_reference, 1,
+    type: NauticNet.Protobuf.WindReference,
+    json_name: "windReference",
+    enum: true
+  )
+
   field(:speed_m_s, 2, type: :float, json_name: "speedMS")
   field(:angle_rad, 3, type: :float, json_name: "angleRad")
 end
