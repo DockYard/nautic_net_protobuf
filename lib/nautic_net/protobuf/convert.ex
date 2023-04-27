1302a389b4cb3dbd0166994afce8eb425c37f97f
Rockwell Schrock <rockwell@schrock.me>
2023-04-27 14:00:13 -0400

    Add and fix some conversions

--- lib/nautic_net/protobuf/convert.ex
+++ lib/nautic_net/protobuf/convert.ex
@@ -13,14 +13,20 @@ defmodule NauticNet.Protobuf.Convert do
   """
   def encode_unit(value, standard_unit, protobuf_unit)
   def encode_unit(value, :rad, :mrad), do: trunc(value * 1000)
-  def encode_unit(value, :m_s, :cm_s), do: trunc(value * 10)
-  def encode_unit(value, :m, :cm), do: trunc(value * 10)
+  def encode_unit(value, :m_s, :cm_s), do: trunc(value * 100)
+  def encode_unit(value, :m, :cm), do: trunc(value * 100)
 
   @doc """
   Convert a Protobuf field back into more-useful units.
   """
   def decode_unit(value, protobuf_unit, standard_unit)
   def decode_unit(value, :mrad, :rad), do: value / 1000
-  def decode_unit(value, :cm_s, :m_s), do: value / 10
-  def decode_unit(value, :cm, :m), do: value / 10
+  def decode_unit(value, :cm_s, :m_s), do: value / 100
+  def decode_unit(value, :cm, :m), do: value / 100
+
+  # :dkn is deciknots: one tenth of a knot
+  def decode_unit(value, :dkn, :m_s), do: value / 10 / 1.94384
+
+  # :ddeg is decidegrees: one tenth of a degree
+  def decode_unit(value, :ddeg, :rad), do: value / 10 * :math.pi() / 180
 end
